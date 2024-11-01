646a247a443b63184a1a030b0553aee85a702d73
Facundo Farias <facundo.farias@trustedmediabrands.com>
2024-11-01 08:08:44 -0300

    CCE-4306: Fix indentation per WPCS

--- src/admin/utils.php
+++ src/admin/utils.php
@@ -3,124 +3,124 @@
 use MeiliSearch\Client;
 
 function get_meilisearch_index(){
-    $meilisearch_options = get_option( 'meilisearch_option_name' );
-    $client = new Client( $meilisearch_options['meilisearch_url_0'], $meilisearch_options['meilisearch_private_key_1'] );
-    try {
-        $index = $client->index( $meilisearch_options['meilisearch_index_name'] );
-    } catch( Exception $e ) {
-        $index = $client->createIndex( $meilisearch_options['meilisearch_index_name'] );
-    }
-    return $index;
+	$meilisearch_options = get_option( 'meilisearch_option_name' );
+	$client = new Client( $meilisearch_options['meilisearch_url_0'], $meilisearch_options['meilisearch_private_key_1'] );
+	try {
+		$index = $client->index( $meilisearch_options['meilisearch_index_name'] );
+	} catch( Exception $e ) {
+		$index = $client->createIndex( $meilisearch_options['meilisearch_index_name'] );
+	}
+	return $index;
 }
 
 function index_post_after_update( $post_ID, $post, $update ) {
-    index_post( $post );
+	index_post( $post );
 }
 
 function index_post_after_meta_update( $post, $request ) {
-    index_post( $post );
+	index_post( $post );
 }
 
 function index_post( $post ){
-    $index    = get_meilisearch_index();
-    $document = apply_filters( 'post_to_document', array(), $post );
-    if ( $document ) {
-        $index->addDocuments($document);
-    }
+	$index    = get_meilisearch_index();
+	$document = apply_filters( 'post_to_document', array(), $post );
+	if ( $document ) {
+		$index->addDocuments($document);
+	}
 }
 
 function delete_post_from_index($post_id){
-    $index = get_meilisearch_index();
-    $index->deleteDocument($post_id);
+	$index = get_meilisearch_index();
+	$index->deleteDocument($post_id);
 }
 
 function index_all_posts( $sync = false ){
-    $post_types = get_post_types_by_support( 'meilisearch' );
-    foreach ( $post_types as $post_type ) {
-        index_all_posts_of_type( $post_type, $sync );
-    }
+	$post_types = get_post_types_by_support( 'meilisearch' );
+	foreach ( $post_types as $post_type ) {
+		index_all_posts_of_type( $post_type, $sync );
+	}
 }
 
 function index_all_posts_of_type( $post_type, $sync = false, $batch_size = -1, $current_page = 1 ) {
-    $index = get_meilisearch_index();
+	$index = get_meilisearch_index();
 
-    $query_args = array(
-        'post_type'      => $post_type,
-        'post_status'    => 'publish',
-        'fields'         => 'all',
-    );
+	$query_args = array(
+		'post_type'      => $post_type,
+		'post_status'    => 'publish',
+		'fields'         => 'all',
+	);
 
-    // Use batch size if provided, otherwise fetch all posts.
-    if ( $batch_size > 0 ) {
-        $query_args['posts_per_page'] = $batch_size;
-        $query_args['paged']          = $current_page;
-    } else {
-        $query_args['posts_per_page'] = -1;
-    }
+	// Use batch size if provided, otherwise fetch all posts.
+	if ( $batch_size > 0 ) {
+		$query_args['posts_per_page'] = $batch_size;
+		$query_args['paged']          = $current_page;
+	} else {
+		$query_args['posts_per_page'] = -1;
+	}
 
-    $query = new WP_Query( $query_args );
-    
-    $documents = array_reduce( $query->posts, function( $carry, $post ) {
-        $document = apply_filters( 'post_to_document', $post );
-        if ( $document ) {
-            $carry[] = $document;
-        }
-        return $carry;
-    }, [] );
+	$query = new WP_Query( $query_args );
+	
+	$documents = array_reduce( $query->posts, function( $carry, $post ) {
+		$document = apply_filters( 'post_to_document', $post );
+		if ( $document ) {
+			$carry[] = $document;
+		}
+		return $carry;
+	}, [] );
 
-    if ( empty( $documents ) ) {
-        return;
-    }
+	if ( empty( $documents ) ) {
+		return;
+	}
 
-    try {
-        $update = $index->addDocuments($documents);
-        
-        if ($sync) {
-            $index->waitForTask($update['taskUid']);
-        }
-    } catch (Exception $e) {
-        error_log('Meilisearch indexing error: ' . $e->getMessage());
-    }
+	try {
+		$update = $index->addDocuments($documents);
+		
+		if ($sync) {
+			$index->waitForTask($update['taskUid']);
+		}
+	} catch (Exception $e) {
+		error_log('Meilisearch indexing error: ' . $e->getMessage());
+	}
 
-    wp_reset_postdata();
+	wp_reset_postdata();
 }
 
 function delete_index(){
-    $index = get_meilisearch_index();
-    $index->delete();
+	$index = get_meilisearch_index();
+	$index->delete();
 }
 
 function count_indexed(){
-    $index = get_meilisearch_index();
-    try {
-        $count = $index->stats();
-        return $count['numberOfDocuments'];
-    } catch( Exception $e ) {
-        return 0;
-    }
+	$index = get_meilisearch_index();
+	try {
+		$count = $index->stats();
+		return $count['numberOfDocuments'];
+	} catch( Exception $e ) {
+		return 0;
+	}
 }
 
 function post_to_document( $document, $post ) {
-    if ( ! $post instanceof WP_Post ) {
-        return false;
-    }
-    if ( $post->post_status !== 'publish' ) {
-        return false;
-    }
-    $categories = [];
-    foreach ( $post->post_category as $category ) {
-        array_push( $categories, get_cat_name( $category ) );
-    }
-    $document = [
-            'id'         => $post->ID,
-            'title'      => $post->post_title,
-            'content'    => strip_tags( $post->post_content ),
-            'img'        => get_the_post_thumbnail_url( $post, array( 100, 100 ) ),
-            'url'        => get_the_permalink( $post ),
-            'tags'       => $post->tags_input,
-            'categories' => $categories,
-            'type'       => $post->post_type,
-    ];
-    return $document;
+	if ( ! $post instanceof WP_Post ) {
+		return false;
+	}
+	if ( $post->post_status !== 'publish' ) {
+		return false;
+	}
+	$categories = [];
+	foreach ( $post->post_category as $category ) {
+		array_push( $categories, get_cat_name( $category ) );
+	}
+	$document = [
+			'id'         => $post->ID,
+			'title'      => $post->post_title,
+			'content'    => strip_tags( $post->post_content ),
+			'img'        => get_the_post_thumbnail_url( $post, array( 100, 100 ) ),
+			'url'        => get_the_permalink( $post ),
+			'tags'       => $post->tags_input,
+			'categories' => $categories,
+			'type'       => $post->post_type,
+	];
+	return $document;
 }
 add_filter( 'post_to_document', 'post_to_document', 10, 2 );
